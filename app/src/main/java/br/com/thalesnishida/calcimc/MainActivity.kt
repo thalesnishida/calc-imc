c891690d18e5d18600f56ad8019bccb6e004f0b2
Thales Nishida <medeiros.tn@gmail.com>
2023-12-18 14:46:01 -0300

    feat: create new activity and function button for linearLayout

--- app/src/main/java/br/com/thalesnishida/calcimc/MainActivity.kt
+++ app/src/main/java/br/com/thalesnishida/calcimc/MainActivity.kt
@@ -1,11 +1,22 @@
 package br.com.thalesnishida.calcimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnImc: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+
+        btnImc.setOnClickListener {
+            val i = Intent(this, imcActivity::class.java)
+            startActivity(i)
+        }
     }
 }
---
+++ app/src/main/java/br/com/thalesnishida/calcimc/imcActivity.kt
@@ -0,0 +1,11 @@
+package br.com.thalesnishida.calcimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class imcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
+    }
+}
